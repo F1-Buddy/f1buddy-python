8ce8ca2b27efce2bb93d9623897f7bad318706cc
rakib-shahid <rakibshahid1298@gmail.com>
2023-01-02 08:03:39 -0500

    fixed thumbnail

--- main.py
+++ main.py
@@ -24,21 +24,23 @@ tree = app_commands.CommandTree(client)
 # Next command
 @tree.command(name="schedule", description="Get race schedule")
 async def schedule_command(interaction):
+    message_embed= discord.Embed(title="Race Schedule", description="")
     schedule2022 = fastf1.get_event_schedule(2022, include_testing=False)
     next_event = 0
     now = pd.Timestamp.now()
+    test_time = pd.Timestamp(year=2021,month=1,day=1)
     out_string = ""
 
     for i in range(len(schedule2022)):
         if schedule2022.iloc[i].values[4] < now:
             next_event = i+1
     try:
-        out_string = ('Next event is the ',schedule2022.iloc[next_event].values[3])
+        out_string = '{} {}'.format('Next event is the ',schedule2022.iloc[next_event].values[3])
     except IndexError:
         out_string = ('It is currently off season! :crying_cat_face:')
-    message_embed= discord.Embed(title="Race Schedule", description=out_string)
-    message_embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/1059409950016811048/1059450769423736883/f1_python_pfp.png')
-    message_embed.set_image(url='https://media.tenor.com/kdIoxRG4W4QAAAAC/crying-crying-kid.gif')
+        message_embed.set_image(url='https://media.tenor.com/kdIoxRG4W4QAAAAC/crying-crying-kid.gif')
+    message_embed.description = out_string
+    message_embed.set_thumbnail(url='https://github.com/F1-Buddy/f1buddy-python/blob/dev-rakib/botPics/f1python128.png')
     await interaction.response.send_message(embed=message_embed)
 
 
