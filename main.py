a70d723ef850d45fc1d4d97f8a005b472b41bc0c
rakib-shahid <rakibshahid1298@gmail.com>
2023-01-04 09:22:01 -0500

    basic next command

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import discord
 import pandas as pd
 from discord import app_commands
 from fastf1 import plotting
-from matplotlib import pyplot as plt
-
+# from matplotlib import pyplot as plt
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
 
 fastf1.Cache.enable_cache('cache/')
 plotting.setup_mpl()
@@ -22,6 +23,61 @@ tree = app_commands.CommandTree(client)
 
 # Next Command
 
+
+def test_next():
+    # now = pd.Timestamp.now()
+    # message_embed = discord.Embed(title="Race Schedule", description="")
+    schedule = fastf1.get_event_schedule(2022, include_testing=False)
+
+    next_event = 0
+    test_time = pd.Timestamp(year=2022, month=9, day=3)
+    out_string = ""
+
+    
+    for i in range(len(schedule)):
+        if schedule.iloc[i].values[4] < test_time:
+            next_event = i+1
+    try:
+        
+       
+        # TIME IS IN LOCAL NOT UTC
+        date_object = schedule.iloc[next_event].values[16]
+        # print('race start = ',date_object, ' local time')
+        # session = fastf1.get_session(date_object.year,schedule.iloc[next_event].values[0],'R')
+        g = Nominatim(user_agent='f1pythonbottesting')
+        location = schedule.iloc[next_event].values[2]
+        coords = g.geocode(location)
+        # print(coords)
+        tf = TimezoneFinder()
+        tz = tf.timezone_at(lng=coords.longitude, lat=coords.latitude)
+        date_object = date_object.tz_localize(tz).tz_convert('America/New_York')
+
+        date = str(schedule.iloc[next_event].values[4].month) + "/"+ str(schedule.iloc[next_event].values[4].day)+"/"+ str(schedule.iloc[next_event].values[4].year)
+        time = str(date_object)[str(date_object).index(":")-2:]
+        time = time[:time.index("-")]
+        # print(date_object)
+        # print(date_object.tz_convert('America/New_York'))
+        # print(tz)
+        # session.load()
+        # print(session.date)
+        # print(date_object)
+        # print('country = ',schedule.iloc[next_event].values[1])
+        # print('location = ',schedule.iloc[next_event].values[2])
+
+        out_string = ''.join([
+            'Next event is the \n',
+            str(schedule.iloc[next_event].values[3]),
+            '\non **',
+            date,
+            '**\nat **',
+            time,
+            " EST** "
+        ])
+    except IndexError:
+        out_string = ('It is currently off season! :crying_cat_face:')
+    print(out_string)
+
+test_next()
 
 @tree.command(name="schedule", description="Get race schedule")
 async def schedule_command(interaction):
@@ -48,10 +104,32 @@ async def schedule_command(interaction):
         if schedule.iloc[i].values[4] < now:
             next_event = i+1
     try:
+        # TIME IS IN LOCAL NOT UTC
+        date_object = schedule.iloc[next_event].values[16]
+        # print('race start = ',date_object, ' local time')
+        # session = fastf1.get_session(date_object.year,schedule.iloc[next_event].values[0],'R')
+        g = Nominatim(user_agent='f1pythonbottesting')
+        location = schedule.iloc[next_event].values[2]
+        #   get lat and long of the race location
+        coords = g.geocode(location)
+        # print(coords)
+        #   find timezone using those coordinates
+        tf = TimezoneFinder()
+        tz = tf.timezone_at(lng=coords.longitude, lat=coords.latitude)
+        date_object = date_object.tz_localize(tz).tz_convert('America/New_York')
 
         date = str(schedule.iloc[next_event].values[4].month) + "/"+ str(schedule.iloc[next_event].values[4].day)+"/"+ str(schedule.iloc[next_event].values[4].year)
-        time = str(schedule.iloc[next_event].values[4])[str(schedule.iloc[next_event].values[4]).index(":")-2:]
-        print(time)
+        time = str(date_object)[str(date_object).index(":")-2:]
+        time = time[:time.index("-")]
+        # print(date_object)
+        # print(date_object.tz_convert('America/New_York'))
+        # print(tz)
+        # session.load()
+        # print(session.date)
+        # print(date_object)
+        # print('country = ',schedule.iloc[next_event].values[1])
+        # print('location = ',schedule.iloc[next_event].values[2])
+
         out_string = ''.join([
             'Next event is the \n',
             str(schedule.iloc[next_event].values[3]),
@@ -59,7 +137,7 @@ async def schedule_command(interaction):
             date,
             '**\nat **',
             time,
-            "**"
+            " EST** "
         ])
     except IndexError:
         out_string = ('It is currently off season! :crying_cat_face:')
