87e2b3bb1b4420537203c927df3108eee03d042e
rakib-shahid <rakibshahid1298@gmail.com>
2023-02-26 23:51:45 -0500

    schedule (probably) fixed

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -22,11 +22,6 @@ class Schedule(commands.Cog):
     async def on_ready(self):
         print('Schedule cog loaded')
 
-    # @commands.command()
-    # async def sync(self, ctx) -> None:
-    #     fmt = await ctx.bot.tree.sync(guild=ctx.guild)
-    #     await ctx.send(f'Synced {len(fmt)}')
-
     @app_commands.command(name='schedule', description='get race schedule')
     async def schedule(self, interaction: discord.Interaction):
         # defer response
@@ -42,9 +37,12 @@ class Schedule(commands.Cog):
         schedule = fastf1.get_event_schedule(2022, include_testing=False)
 
         # index of next event (round number)
-        next_event = 0
+        next_event = 2
+        ################################################################
         # timestamp to test function
-        # now = pd.Timestamp(year=2022, month=10, day=27)
+        # now = pd.Timestamp(year=2022, month=10, day=31)
+        ################################################################
+        
         
         # string to hold final message
         out_string = "It is currently " + now.strftime('%Y-%m-%d %X') + "\n\n"
@@ -52,48 +50,56 @@ class Schedule(commands.Cog):
         # range starts at 2 because I skip 0 and 1 since I ignore preseason testing sessions
         # find round number of next event
         for i in range(2,len(schedule)):
+            next_event = i+1
             # if (fp1 < now < race) --> scenario where today is before the next race but after this weekend's fp1
             # or
             # if (fp1 > now > last race) --> scenario where today is after the last race but before upcoming race weekend's fp1
             if ((schedule.loc[i,"Session1Date"] < now) and (schedule.loc[i,"Session5Date"] > now)) or ((schedule.loc[i,"Session1Date"] > now) and (schedule.loc[i-1,"Session5Date"] < now)):
-                # temp_race_name = schedule.loc[i,"EventName"]
-                # print("\n"+temp_race_name + " FP1 < now < "+temp_race_name+"?")
-                # print(((schedule.loc[i,"Session1Date"] < now) and (schedule.loc[i,"Session5Date"] > now)))
-                # print(temp_race_name + " FP1 > now > "+schedule.loc[i-1,"EventName"]+"?")
-                # print(((schedule.loc[i,"Session1Date"] > now) and (schedule.loc[i-1,"Session5Date"] < now)))
-                next_event = i
-
-
-        # print(schedule.loc[i,"Session1Date"])
-        # print(now)
+                next_event -= 1
+                # testing
+                temp_race_name = schedule.loc[i,"EventName"]
+                print("\n"+temp_race_name + " FP1 < now < "+temp_race_name+"?")
+                print(((schedule.loc[i,"Session1Date"] < now) and (schedule.loc[i,"Session5Date"] > now)))
+                print(temp_race_name + " FP1 > now > "+schedule.loc[i-1,"EventName"]+"?")
+                print(((schedule.loc[i,"Session1Date"] > now) and (schedule.loc[i-1,"Session5Date"] < now)))
+                break
         
-
-        # gets session times for weekend
-        # session_times = {
-        #     "fp1_time": schedule.iloc[next_event].values[8],
-        #     "fp2_time": schedule.iloc[next_event].values[10],
-        #     "fp3_time": schedule.iloc[next_event].values[12],
-        #     "quali_time": schedule.iloc[next_event].values[14],
-        #     "race_time": schedule.iloc[next_event].values[16]
-        # }
-        # print(session_times)
+        
         
         try:      
+            # check if it is off season
+            if (len(schedule) <= next_event):
+                raise IndexError
+            
+            # else
             # get name of event
             race_name = schedule.loc[next_event,"EventName"]
-            # print(race_name)
+            
             # get emoji for country
             emoji = ":flag_"+(coco.convert(names=schedule.loc[next_event,"Country"],to='ISO2')).lower()+":"
+            
             # Rename embed title
             message_embed.title = "Race Schedule for "+emoji+"**" + race_name + "**" + emoji  
+            
             # create a dictionary to store converted times
-            converted_session_times = {
-                ":one: FP1": schedule.loc[next_event, "Session1Date"],
-                ":two: FP2": schedule.loc[next_event, "Session2Date"],
-                ":three: FP3": schedule.loc[next_event, "Session3Date"],
-                ":stopwatch: Qualifying": schedule.loc[next_event, "Session4Date"],
-                ":checkered_flag: Race": schedule.loc[next_event, "Session5Date"]
-            }
+            # adjust emojis/message according to weekend format
+            if (schedule.loc[next_event,"EventFormat"] == 'conventional'):
+                converted_session_times = {
+                    ":one: FP1": schedule.loc[next_event, "Session1Date"],
+                    ":two: FP2": schedule.loc[next_event, "Session2Date"],
+                    ":three: FP3": schedule.loc[next_event, "Session3Date"],
+                    ":stopwatch: Qualifying": schedule.loc[next_event, "Session4Date"],
+                    ":checkered_flag: Race": schedule.loc[next_event, "Session5Date"]
+                }
+            else:
+                converted_session_times = {
+                    ":one: FP1": schedule.loc[next_event, "Session1Date"],
+                    ":two: FP2": schedule.loc[next_event, "Session2Date"],
+                    ":stopwatch: Qualifying": schedule.loc[next_event, "Session3Date"],
+                    ":race_car: Sprint": schedule.loc[next_event, "Session4Date"],
+                    ":checkered_flag: Race": schedule.loc[next_event, "Session5Date"]
+                }
+            
 
             # TIME IS IN LOCAL NOT UTC
             # create coordinate finder object
@@ -122,10 +128,12 @@ class Schedule(commands.Cog):
             for key in converted_session_times.keys():
                 out_string += key + ": \t`" + (converted_session_times.get(key)).strftime('%Y/%m/%d %X') + "`\n"
 
-        except:
+        except IndexError:
             out_string = ('It is currently off season! :crying_cat_face:')
             message_embed.set_image(url='https://media.tenor.com/kdIoxRG4W4QAAAAC/crying-crying-kid.gif')
             message_embed.set_footer(text = "*probably*")
+        except:
+            out_string = "Unknown error occured! Uh-Oh! Bad! :thermometer_face:"
 
         #######################################################################
         # add final string to embed and send it
