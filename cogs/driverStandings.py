f41d23aece68bb638b5a12aadfb29f143a11aa95
anuragcar <akar01@nyit.edu>
2023-03-04 22:37:13 -0500

    emoji implementation

--- cogs/driverStandings.py
+++ cogs/driverStandings.py
@@ -9,18 +9,18 @@ from discord import app_commands
 from discord.ext import commands
 now = pd.Timestamp.now()
 driver_name, driver_position, driver_points = [], [], []
-# team_emojis = {
-#     "Red Bull":"<:rb:1081767515790770247>",
-#     "Mercedes":"<:merc:1081767514620571749>",
-#     "Ferrari":"<:sf:1081767510019411978>",
-#     "McLaren":"<:mcl:1081767512733126736>",
-#     "Alpine F1 Team":"<:alp:1081767507209224192>",
-#     "Aston Martin":"<:ast:1081767508287176734>",
-#     "Alfa Romeo":"<:ar:1081767504617148417>",
-#     "AlphaTauri":"<:at:1081767505539903508>",
-#     "Williams":"<:w_:1081767613283176579>",
-#     "Haas F1 Team":"<:haas:1081767511424520313>"
-# }
+team_emojis = {
+    "Red Bull":"<:rb:1081767515790770247>",
+    "Mercedes":"<:merc:1081767514620571749>",
+    "Ferrari":"<:sf:1081767510019411978>",
+    "McLaren":"<:mcl:1081767512733126736>",
+    "Alpine F1 Team":"<:alp:1081767507209224192>",
+    "Aston Martin":"<:ast:1081767508287176734>",
+    "Alfa Romeo":"<:ar:1081767504617148417>",
+    "AlphaTauri":"<:at:1081767505539903508>",
+    "Williams":"<:w_:1081767613283176579>",
+    "Haas F1 Team":"<:haas:1081767511424520313>"
+                }
 
 
 class driverStandings(commands.Cog):
@@ -41,11 +41,10 @@ class driverStandings(commands.Cog):
         message_embed = discord.Embed(title=f"{year} Driver Standings", description="").set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
         message_embed.colour = discord.Colour.dark_red()
         
-        emojis = []
-        for i in guild:
-            for j in guild.emojis:
-                emojis.append(j)
-        print(emojis)
+        print("emojis:")
+        for i in self.bot.emojis:
+            print(i)
+            
         for i in range(0,20):
             driver_standings = (response['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][i])
             driver_data = (response['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][i]['Driver'])
