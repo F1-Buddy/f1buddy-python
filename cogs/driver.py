a458982d0ca86c4313e03f56c8a0d750a669139c
rakib-shahid <rakibshahid1298@gmail.com>
2023-03-06 13:00:38 -0500

    worked on drivers only works for 23 grid

--- cogs/driver.py
+++ cogs/driver.py
@@ -7,9 +7,20 @@ import json
 # import pandas as pd
 from discord import app_commands
 from discord.ext import commands
+from lib.drivernames import driver_names
+import country_converter as coco
 # from geopy.geocoders import Nominatim
 # from timezonefinder import TimezoneFinder
 # import country_converter as coco
+
+stat_map = {
+    'Starts':'starts',
+    'Career Points': 'careerpoints',
+    'Wins':'wins',
+    'Podiums':'podiums',
+    'Poles':'poles',
+    'Fastest Laps':'fastestlaps',    
+}
 
 class Driver(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -18,10 +29,6 @@ class Driver(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Driver cog loaded')
-
-    driver_list = [
-        
-    ]
 
     @app_commands.command(name='driver', description='Get driver info')
     @app_commands.describe(driver = "Driver name")
@@ -36,10 +43,12 @@ class Driver(commands.Cog):
         message_embed = discord.Embed(title="temp_driver_title", description="")
         message_embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
         message_embed.colour = discord.Colour.dark_red()
+        description_string = ''
         
         # get wikipedia article
         wk = mediawiki.MediaWiki()
         driver_article = wk.page(title=driver,auto_suggest=True)
+        # emoji = ":flag_" + (coco.convert(names='', to='ISO2')).lower()+":"
 
         # get driver image
         article_url = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=" + (str)(driver_article.title)
@@ -47,16 +56,24 @@ class Driver(commands.Cog):
         response = json.loads(article_json.content)
         driver_image_url = (response['query']['pages'][driver_article.pageid]['original']['source'])
 
+        # get other driver info like stats
+        driver_full_name = driver_article.title
+        driver_info = json.loads(requests.get("https://ergast.com/api/f1/drivers/"+(str)(driver_full_name[driver_full_name.index(' ')+1:])+".json").content)
+        # print(driver_info)
+        driver_code = driver_info["MRData"]["DriverTable"]["Drivers"][0]["code"]
+        # print(driver_code)
+        for x in stat_map:
+            stat_url = ("https://en.wikipedia.org/w/api.php?action=expandtemplates&format=json&text={{F1stat|"
+            +driver_code+"|"+ stat_map[x] +"}}&prop=wikitext")
+            print(stat_url)
+            
+            stat_json = json.loads(requests.get(stat_url).content)
+            description_string += x+": **"+stat_json['expandtemplates']['wikitext'] + "**\n"
 
-
-        # page_html = driver_article.html
-        # image_index = driver_article.html.index("class=\"infobox-image\">")
-        # page_html = page_html[image_index:image_index+61]
-        # print(driver_article.html.index("class=\"infobox-image\">"))
-        # print(page_html)
 
         message_embed.set_image(url=driver_image_url)
-        message_embed.title = driver_article.title
+        message_embed.title = driver_full_name
+        message_embed.description = description_string
         
 
 
