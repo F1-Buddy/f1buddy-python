7e03e58eef3de6929ba01cce1ec4aefc3143bebd
anuragcar <akar01@nyit.edu>
2023-06-26 11:53:12 -0400

    rewrote wdc to use fastf1

--- cogs/driver_standings.py
+++ cogs/driver_standings.py
@@ -1,8 +1,10 @@
+import datetime
 import discord
 import requests
 import json
 import typing
 import pandas as pd
+from fastf1.ergast import Ergast
 from discord import app_commands
 from discord.ext import commands
 from lib.emojiid import team_emoji_ids
@@ -22,49 +24,40 @@ class DriverStandings(commands.Cog):
     async def DriverStandings(self, interaction: discord.Interaction, year: typing.Optional[int]):
         await interaction.response.defer()
         driver_name, driver_position, driver_points = [], [], []
-        # get standings JSON
-        url = "https://ergast.com/api/f1/current/driverStandings.json" if (year == None) or (year < 1957) or (year >= now.year) else f"https://ergast.com/api/f1/{year}/driverStandings.json"
-        # print(url)
-        ##############################################################################
-        # use this for TESTING ONLY, for use until ergast fixes their SSL certificate
-        # constructorStandings = requests.get(url,verify=False)
-        ##############################################################################
-        driverStandings = requests.get(url)
-        response = json.loads(driverStandings.content)
-        driver_total = (int)(response['MRData']['total'])
-        # get season year
-        year = (response['MRData']['StandingsTable']['season']) 
-        # set embed color and title
+        ergast = Ergast()
+        year = datetime.datetime.now().year if (year == None) or (year < 1957) or (year >= now.year) else year # set year depending on input
+                
+        # go through each
+        driver_standings = ergast.get_driver_standings(season=year).content[0]
+        for index in range(len(driver_standings)):
+            position = driver_standings.iloc[index]['position']
+            name = f"{driver_standings.iloc[index]['givenName']} {driver_standings.iloc[index]['familyName']}"
+            points = driver_standings.iloc[index]['points']
+            driver_position.append(position)
+            driver_points.append(points)
+            constructor_names = driver_standings.iloc[index]['constructorNames']
+            constructor_names_string = '\n'.join(map(str, constructor_names))
+            try:
+                driver_name.append((str)(self.bot.get_emoji(team_emoji_ids[constructor_names_string])) + " " + name)
+            except:
+                driver_name.append(name)
+        
+        # sets embed color and title
         message_embed = discord.Embed(title=f"{year} Driver Standings", description="").set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
         message_embed.set_author(name='f1buddy',icon_url='https://raw.githubusercontent.com/F1-Buddy/f1buddy-python/main/botPics/f1pythonpfp.png')
         message_embed.colour = colors.default
-            
-        for i in range(0,driver_total):
-            driver_standings = (response['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][i])
-            driver_data = (response['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][i]['Driver'])
-            driver_constructor = (response['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][i]['Constructors'][0])
-                
-            try:
-                if (driver_total <= 24):
-                    driver_name.append((str)(self.bot.get_emoji(team_emoji_ids[driver_constructor['name']]))+ ' ' + driver_data['givenName'] + ' ' + driver_data['familyName'])
-                else:
-                    driver_name.append((str)(self.bot.get_emoji(team_emoji_ids[driver_constructor['name']]))+ ' ' + driver_data['givenName'][0] + "." + ' ' + driver_data['familyName'])
-            except:
-                driver_name.append((driver_data['givenName']) + ' ' + (driver_data['familyName']))
 
-            driver_position.append(driver_standings['position'])
-            driver_points.append(driver_standings['points'])
-        
-        driver_positions_string = '\n'.join(driver_position)
+        # replaces commas in list with newline
+        driver_positions_string = '\n'.join(map(str, driver_position))
         driver_names_string = '\n'.join(driver_name)
-        if (len(driver_names_string) >= 1024):
+        if len(driver_names_string) >= 1024:
             driver_names_string = driver_names_string[:1024 - len(driver_names_string) - 1] + '.'
-        driver_points_string = '\n'.join(driver_points)
+        driver_points_string = '\n'.join(map(str, driver_points))
         
+        # discord embed columns
         message_embed.add_field(name="Position", value= driver_positions_string,inline=True)
         message_embed.add_field(name="Driver", value=driver_names_string,inline=True)
         message_embed.add_field(name="Points", value=driver_points_string,inline=True)
-        # send final embed
         await interaction.followup.send(embed=message_embed)
 
 async def setup(bot):
