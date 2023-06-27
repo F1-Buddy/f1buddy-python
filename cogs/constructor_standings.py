c713fca779e3340de54e45ecce99f823d700446d
anuragcar <akar01@nyit.edu>
2023-06-26 21:05:26 -0400

    changed wcc to fastf1

--- cogs/constructor_standings.py
+++ cogs/constructor_standings.py
@@ -1,8 +1,10 @@
+import datetime
 import discord
 import requests
 import json
 import typing
 import pandas as pd
+from fastf1.ergast import Ergast
 from discord import app_commands
 from discord.ext import commands
 from lib.emojiid import team_emoji_ids
@@ -22,40 +24,40 @@ class ConstructorStandings(commands.Cog):
     
     async def ConstructorStandings(self, interaction: discord.Interaction, year: typing.Optional[int]):
         await interaction.response.defer()
+        ergast = Ergast()
         team_names, team_position, team_points = [], [], []
-        url = "https://ergast.com/api/f1/current/constructorStandings.json" if (year == None) or (year < 1957 and year >= now.year) else f"https://ergast.com/api/f1/{year}/constructorStandings.json"
-        # print(url)
-        ##############################################################################
-        # use this for TESTING ONLY, for use until ergast fixes their SSL certificate
-        # constructorStandings = requests.get(url,verify=False)
-        ##############################################################################
-        constructorStandings = requests.get(url)
-        response = json.loads(constructorStandings.content)
-        year = (response['MRData']['StandingsTable']['season']) 
-        constructor_total = (int)(response['MRData']['total'])
+        year = datetime.datetime.now().year if (year == None) or (year < 1957) or (year >= now.year) else year # set year depending on input
+        
+        constructor_standings = ergast.get_constructor_standings(season=year).content[0]
+        for index in range(len(constructor_standings)):
+            position = constructor_standings.iloc[index]['position']
+            points = constructor_standings.iloc[index]['points']
+            if points == int(points):
+                points = int(points)
+            team_position.append(position)
+            team_points.append(points)
+            names = constructor_standings.iloc[index]['constructorName']
+            try:
+                team_names.append((str)(self.bot.get_emoji(team_emoji_ids[names])) + ' ' + names)
+            except:
+                team_names.append(names)
+        
         message_embed = discord.Embed(title=f"{year} Constructor Standings", description="").set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
         message_embed.set_author(name='f1buddy',icon_url='https://raw.githubusercontent.com/F1-Buddy/f1buddy-python/main/botPics/f1pythonpfp.png')
         message_embed.colour = colors.default
         
-        for i in range(0,constructor_total):
-            constructor_standings = (response['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings'][i])
-            constructor_data = (response['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings'][i]['Constructor'])
-            
-            try:
-                team_names.append((str)(self.bot.get_emoji(team_emoji_ids[constructor_data['name']]))+' ' + constructor_data['name'])
-            except:
-                team_names.append(constructor_data['name'])
-                
-            team_position.append(constructor_standings['position'])
-            team_points.append(constructor_standings['points'])
-        
         # Sets McLaren's position to EX, due to their disqualification in the 2007 constructor's championship. 
-        if (year == "2007"):
+        if (year == 2007):
             team_position[10] = "EX"
         
-        message_embed.add_field(name="Position", value='\n'.join(team_position),inline=True)
-        message_embed.add_field(name="Team Name", value='\n'.join(team_names),inline=True)
-        message_embed.add_field(name="Points", value='\n'.join(team_points),inline=True)
+        # replaces commas in list with newline
+        team_position = '\n'.join(map(str, team_position))
+        team_names = '\n'.join(team_names)
+        team_points = '\n'.join(map(str, team_points))
+        
+        message_embed.add_field(name="Position", value=team_position,inline=True)
+        message_embed.add_field(name="Team Name", value=team_names,inline=True)
+        message_embed.add_field(name="Points", value=team_points,inline=True)
         # send final embed
         await interaction.followup.send(embed=message_embed)
 
