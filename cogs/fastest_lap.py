3caf3572cd9fb8d694699179ce5b781dc4e88936
anuragcar <akar01@nyit.edu>
2023-07-14 20:16:51 -0400

    2018 tires, round fix, load fix, remove prints

--- cogs/fastest_lap.py
+++ cogs/fastest_lap.py
@@ -10,7 +10,7 @@ import country_converter as coco
 from lib.emojiid import nation_dictionary
 from discord.ext import commands
 from fastf1.ergast import Ergast
-from lib.emojiid import tire_emoji_ids, team_emoji_ids
+from lib.emojiid import tire_emoji_ids, tire_emoji_ids_2018, team_emoji_ids
 from lib.colors import colors
 now = pd.Timestamp.now()
 nationality_dict = nation_dictionary()
@@ -18,7 +18,7 @@ current_year = datetime.date.today().year
 
 def fastest_lap_info(self, round_num, year, driver_name, driver_laptime, tyre_age, grand_prix):
     session = fastf1.get_session(year, round_num, 'Race')
-    session.load()
+    session.load(laps=True,telemetry=False,weather=False,messages=False,livedata=False)
     fastest_lap = session.laps.pick_fastest()
     
     driver = fastest_lap.loc['Driver']
@@ -28,7 +28,10 @@ def fastest_lap_info(self, round_num, year, driver_name, driver_laptime, tyre_ag
     lap_time = (datetime.datetime.min + lap_time).time().strftime('%M:%S.%f')[:-3]
     tyre_life = int(fastest_lap.loc['TyreLife'])
     compound = fastest_lap.loc['Compound']
-    compound = tire_emoji_ids.get(compound)
+    if year <= 2018:
+        compound = tire_emoji_ids_2018.get(compound)
+    else:
+        compound = tire_emoji_ids.get(compound)
     race_name = f"{session.event.EventName}"
     race_name = race_name.replace(" Grand Prix", "")
     
@@ -52,42 +55,37 @@ def fastest_lap_info(self, round_num, year, driver_name, driver_laptime, tyre_ag
     
     
 def get_fastest_lap(self, round, year):
-    print("just year")
+    try:
+        round = int(round)
+    except ValueError:
+        round = round
+        
     driver_name, driver_laptime, tyre_age, grand_prix = [],[],[],[]
     year_sched = fastf1.get_event_schedule(year, include_testing=False)
     num_rounds = year_sched.shape[0]
-    if round == 999:
+    
+    if round == 999: # round==999 when user input for round is none, get all fastest laps
         try: 
-            print("h")
-            print(num_rounds)
             for round_num in range(1, num_rounds + 1):
-                print("ok")
-                print(f"{year} + {round}")
-                print(round_num)
-                print(year_sched)
                 try:
                     sessionTime = year_sched.loc[round_num, "Session4Date"].tz_convert('America/New_York') if year_sched.loc[round_num, "EventFormat"] == 'conventional' else year_sched.loc[round_num, "Session2Date"].tz_convert('America/New_York')
                 except Exception as e:
                     traceback.print_exc()
                     print(f"Error: {e}")
-                    
-                print(sessionTime)
-                print("time")
+
                 if now.tz_localize('America/New_York') < sessionTime:
                     break
                 try:
                     driver_name, driver_laptime, tyre_age, grand_prix = fastest_lap_info(self, round_num, year, driver_name, driver_laptime, tyre_age, grand_prix)
-                    print("dome")
                 except Exception as e:
                     print(f"An error occurred in round {round_num}: {e}")
                     continue
         except Exception as e:
             fastest_lap_embed.set_image(url='https://media.tenor.com/lxJgp-a8MrgAAAAd/laeppa-vika-half-life-alyx.gif')
             fastest_lap_embed.description = f"Error Occured :( {e}"        
-    else:    
+    else: # if round not none, get lap for that round
         try:
             driver_name, driver_laptime, tyre_age, grand_prix = fastest_lap_info(self, round, year,driver_name, driver_laptime, tyre_age, grand_prix)
-            print("ok")
         except Exception as e:
             fastest_lap_embed.set_image(url='https://media.tenor.com/lxJgp-a8MrgAAAAd/laeppa-vika-half-life-alyx.gif')
             fastest_lap_embed.description = f"An error occurred in round {round}: :( {e}"   
@@ -97,12 +95,9 @@ def get_fastest_lap(self, round, year):
     fastest_lap_embed = discord.Embed(title=f"Fastest Lap {year}", description="").set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
     fastest_lap_embed.set_author(name='f1buddy',icon_url='https://raw.githubusercontent.com/F1-Buddy/f1buddy-python/main/botPics/f1pythonpfp.png')
     fastest_lap_embed.colour = colors.default
-    fastest_lap_embed.add_field(name="GP     Name", value=grand_prix,inline=True)     
-    # fastest_lap_embed.add_field(name="Name", value= driver_name,inline=True)
+    fastest_lap_embed.add_field(name="GP     Name", value=tyre_age,inline=True)     
     fastest_lap_embed.add_field(name="Laptime", value=driver_laptime,inline=True)
-    fastest_lap_embed.add_field(name="Age", value=tyre_age,inline=True)
-    # fastest_lap_embed.add_field(name="Lap", value=race_lap,inline=True)
-    # fastest_lap_embed.add_field(name="Age", value=tyre_age,inline=True)
+    fastest_lap_embed.add_field(name="Age", value=grand_prix,inline=True)
     return fastest_lap_embed
      
 class fastest_lap(commands.Cog):
@@ -113,9 +108,9 @@ class fastest_lap(commands.Cog):
         print('Fastest lap cog loaded')
         
     @app_commands.command(name='fl', description='Get fastest lap')
-    @app_commands.describe(round = "Round for fastest lap")
+    @app_commands.describe(round = 'Round name or number (Australia or 3)')
     @app_commands.describe(year = "Year (supports 2018 and after)")
-    async def fastest_lap(self, interaction: discord.Interaction, round: typing.Optional[int], year: typing.Optional[int]):
+    async def fastest_lap(self, interaction: discord.Interaction, round: typing.Optional[str], year: typing.Optional[int]):
         await interaction.response.defer()
         fastest_lap_embed = discord.Embed(title=f"Fastest Lap", description="").set_thumbnail(url='https://cdn.discordapp.com/attachments/884602392249770087/1059464532239581204/f1python128.png')
         fastest_lap_embed.set_author(name='f1buddy',icon_url='https://raw.githubusercontent.com/F1-Buddy/f1buddy-python/main/botPics/f1pythonpfp.png')
@@ -123,17 +118,21 @@ class fastest_lap(commands.Cog):
         loop = asyncio.get_running_loop()
         if round is None:
             round = 999
-            print(round)
         if year is None:
             year = 2023
         try:
             if year < 2018 or year > current_year:
-                raise YearNotValidException(f"Year cannot be before 2018 or after {current_year}!") 
-            if round < 0 or (round > 30 and round != 999):
-                raise RoundNotValidException(f"Enter a valid round number.")
-            
-            print('a') 
-            print(f"{round} + {year}")
+                raise YearNotValidException(f"Year cannot be before 2018 or after {current_year}!")
+            try:
+                event_round = int(round)
+            except ValueError:
+                pass
+            try:
+                if event_round:
+                    if event_round < 0 or (event_round > 30 and event_round != 999):
+                        raise RoundNotValidException(f"Enter a valid round number.")
+            except:
+                pass
             fastest_lap_embed = await loop.run_in_executor(None, get_fastest_lap, self, round, year)
         except YearNotValidException as ynve:
             fastest_lap_embed.set_image(url='https://media.tenor.com/lxJgp-a8MrgAAAAd/laeppa-vika-half-life-alyx.gif')
