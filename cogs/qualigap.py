b305e9f258acaa15b551993bb2fcd3a2564a03d7
anuragcar <akar01@nyit.edu>
2023-10-06 10:38:56 -0400

    add watermark

--- cogs/qualigap.py
+++ cogs/qualigap.py
@@ -7,6 +7,7 @@ from discord import app_commands
 from discord.ext import commands
 from matplotlib import pyplot as plt
 import matplotlib
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 matplotlib.use('agg')
 from lib.colors import colors
 import pandas as pd
@@ -127,6 +128,18 @@ def quali_gap(round, year):
             for label in ax.get_yticklabels():
                 label.set_fontproperties(bold_font)
             plt.rcParams['savefig.dpi'] = 300
+            watermark_img = plt.imread('botPics/f1pythoncircular.png') # set directory for later use
+            try:
+                # add f1buddy pfp
+                watermark_box = OffsetImage(watermark_img, zoom=0.1) 
+                ab = AnnotationBbox(watermark_box, (-0.115,1.205), xycoords='axes fraction', frameon=False)
+                ax.add_artist(ab)
+                # add text next to it
+                ax.text(-0.075,1.195, 'Made by F1Buddy Discord Bot', transform=ax.transAxes,
+                        fontsize=10,fontproperties=bold_font)
+            except Exception as e:
+                print(e)
+            # save plot
             # save plot
             plt.savefig("cogs/plots/qualigap/"+race.date.strftime('%Y-%m-%d_%I%M')+"_QualiGap"+'.png')
             # plt.show()
