8817de5c1259e398e8be1e3e015df13ae04a3d0d
rakib-shahid <rakibshahid1298@gmail.com>
2024-01-09 21:32:41 -0500

    async schedule

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import fastf1
 import pandas as pd
@@ -141,9 +142,11 @@ class Schedulenew(commands.Cog):
     @app_commands.command(name='schedule', description='get F1 race schedule')
     async def schedule(self, interaction: discord.Interaction):
         # defer response
-        await interaction.response.defer()        
-        dc_embed = get_schedule()
+        await interaction.response.defer()     
+        loop = asyncio.get_running_loop()   
+        dc_embed = await loop.run_in_executor(None, get_schedule)
         await interaction.followup.send(embed=dc_embed.embed)
+        loop.close()
         
         
 async def setup(bot):
