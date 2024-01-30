ff31d988f38799a9ae5075c67c5febb1690a4f0a
rakib-shahid <rakibshahid1298@gmail.com>
2024-01-30 00:34:33 -0500

    working on lights command

---
+++ cogs/lightsout.py
@@ -0,0 +1,39 @@
+import asyncio
+import datetime
+import time
+import discord
+import typing
+import pandas as pd
+# from fastf1.ergast import Ergast
+# import fastf1
+from discord import app_commands
+from discord.ext import commands
+# from lib.emojiid import team_emoji_ids
+# from lib.colors import colors
+import repeated.embed as em
+
+
+
+        
+class lightsoutgame(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('lightsoutgame cog loaded')
+
+    @app_commands.command(name='lightsoutgame', description='lightsoutgame')
+    async def schedule(self, interaction: discord.Interaction):
+        # defer response
+        await interaction.response.defer()     
+        msg = await interaction.followup.send(content="hi",wait=True)
+        await print(type(msg))
+        await interaction.edit_original_response(content="hhh")
+        # await print(type(msg2))
+        # await interaction.response.edit_message(content="hello")
+        # loop.close()
+        
+        
+async def setup(bot):
+    await bot.add_cog(lightsoutgame(bot))
