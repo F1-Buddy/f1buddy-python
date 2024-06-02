cae1e5f6e64e3db1235a63435e032ef8f89fdde8
rakib-shahid <rakibshahid1298@gmail.com>
2024-06-01 22:29:42 -0400

    h2h final fix?

--- cogs/h2hnew.py
+++ cogs/h2hnew.py
@@ -102,7 +102,7 @@ def get_data(year, session_type,ignore_dnfs):
             
             # figure out which races to ignore
             both_drivers_finished = True
-            if (ignore_dnfs):
+            if (ignore_dnfs == 'True'):
                 if (session_type.value == 'r'):
                     dnf = ['D','E','W','F','N']
                     for driver in team_results.index:
@@ -274,11 +274,11 @@ class h2hnew(commands.Cog):
     @app_commands.choices(ignore_dnfs=[app_commands.Choice(name="Yes", value="True"), 
                                         app_commands.Choice(name="No", value="False"),])
     
-    async def h2hnew(self, interaction: discord.Interaction, year: typing.Optional[int], session_type: app_commands.Choice[str], ignore_dnfs: app_commands.Choice[bool]):  
+    async def h2hnew(self, interaction: discord.Interaction, year: typing.Optional[int], session_type: app_commands.Choice[str], ignore_dnfs: app_commands.Choice[str]):  
         await interaction.response.defer()
         loop = asyncio.get_running_loop()
         # await interaction.followup.send(content='h2h')
-        dc_embed, file = await loop.run_in_executor(None, get_embed, self, year, session_type, bool(ignore_dnfs.value))
+        dc_embed, file = await loop.run_in_executor(None, get_embed, self, year, session_type, ignore_dnfs.value)
         
         if not (file is None):
             await interaction.followup.send(embed = dc_embed.embed, file=file)
